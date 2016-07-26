6e5c40fc97126959fb86f37a5f37dacf265eeab9
brandonisthebomb <liu.b97@gmail.com>
2016-07-26 08:12:41 -0400

    Add GoogleMap

--- app/src/main/java/liu/brandon/wahoowalkfaster/MainActivity.java
+++ app/src/main/java/liu/brandon/wahoowalkfaster/MainActivity.java
@@ -6,6 +6,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -18,23 +23,28 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends Activity implements Callback<TranslocResponse> {
+public class MainActivity extends Activity implements Callback<TranslocResponse>, OnMapReadyCallback {
 
     private final static String TAG = "MainActivity";
+
     private String agencyId = "347"; // UVA
+    private List<Stop> stops;
+
+    private MapFragment mMapFragment;
+    private GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadData();
+
+        mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
     }
 
     private void loadData() {
-
-    }
-
-    public void onClick(View view) {
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
                 .create();
@@ -74,5 +84,12 @@ public class MainActivity extends Activity implements Callback<TranslocResponse>
     public void onFailure(Call<TranslocResponse> call, Throwable t) {
         Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "Query failed: " + call.toString());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+            .position(new LatLng(0,0))
+            .title("Marker"));
     }
 }
